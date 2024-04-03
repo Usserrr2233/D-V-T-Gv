4f44e864e540880472603f2e035834f238966824
Usserrr2233 <163590768+Usserrr2233@users.noreply.github.com>
2024-04-03 11:08:22 +0000

    Update config.js

--- config.js
+++ config.js
@@ -3,21 +3,21 @@ module.exports = {
     * @title Guild Id
     * @description The guild id of the server
     */
-   guildId: "",
+   guildId: "1147205454901026816",
    /**
     * @title Role Id
     * @description The role to give the server members who have the phrase
     */
-   roleId: "",
+   roleId: "1147206761015676978",
    /**
     * @title Phrase
     * @description The phrase to trigger the role
     */
-   phrase: "@ALEXA#0114",
+   phrase: "⟬ Dᴏᴜᴅᴜᴜ ﹗OFF ⟭",
    /**
     * @title Channel Id
     * @description The channel id to send the logs to
     * Currently in a work in progress
     */
-   channelId: "",
+   channelId: "1152043616705982484",
 }
